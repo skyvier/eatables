11da387f1dde0e0a5a5825074ccd0b4b4b5333ea
Joonas Laukka <joonas.laukka@helsinki.fi>
2015-07-31 21:45:09 +0300

    added a quick development mode

--- src/app.js
+++ src/app.js
@@ -11,6 +11,11 @@ if(!db.init()) {
    console.log("db success");
 }
 
+var dev = function (input) {
+   if(process.argv[2] === '-dev')
+      console.log(input);
+};
+
 server.listen(1337);
 
 app.use(express.static('public'));
@@ -25,43 +30,47 @@ app.post('/error', function (req, res) {
 });
 
 io.on('connection', function (socket) {
+   /* Display connection information */
+   var address = socket.handshake.address;
+   console.log("\n### CONNECTION ###\nFROM: " + address);
+
    /* This won't be needed in the future */
    socket.on('query', function (data) {
-      console.log("\n### QUERY REQUEST ###");
+      dev("\n### QUERY REQUEST ###");
 
       if(!data) {
-         console.log("### EMPTY DATA PACKET ###");
+         dev("### EMPTY DATA PACKET ###");
          return;
       } 
 
       db.query(data.options, data, function (err, res) {
          if(err) {
-            console.log(err);
+            dev(err);
             return;
          }
 
-         console.log("\n### RESPONSE ###");
-         console.log(res);
+         dev("\n### RESPONSE ###");
+         dev(res);
          socket.emit('query_response', res);
       });
    });
 
    socket.on('insert', function (data) {
-      console.log("\n### INSERT REQUEST ###");
+      dev("\n### INSERT REQUEST ###");
 
       if(!data) {
-         console.log("### EMPTY DATA PACKET ###");
+         dev("### EMPTY DATA PACKET ###");
          return;
       }
 
       db.insert(data, function (err, res) {
          if(err) {
-            console.log(err);
+            dev(err);
             return;
          }
 
-         console.log("\n### RESPONSE ###");
-         console.log(res.result);
+         dev("\n### RESPONSE ###");
+         dev(res.result);
          socket.emit('insert_response', res.result);
       });
    });
